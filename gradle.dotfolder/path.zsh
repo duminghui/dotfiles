1e166fc375085a8ff7ce52d3e036842f79b5f87b
duminghui <duminghui@users.noreply.github.com>
2016-07-09 01:24:34 +0800

    init project

---
+++ gradle.dotfolder/path.zsh
@@ -0,0 +1,2 @@
+export GRADLE_HOME=/usr/local/opt/gradle
+export PATH=$GRADLE_HOME/bin:$PATH
---
+++ java/path.zsh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk.latest/Contents/Home
+export PATH=$JAVA_HOME/bin:$PATH
+export GROOVY_HOME=/usr/local/opt/groovy/libexec
+export PATH=$GROOVY_HOME/bin:$PATH
