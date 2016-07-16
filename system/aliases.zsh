80bb96056b41587d58437a4c5bb0beca3a178130
duminghui <dumh@jolgoo.com>
2016-07-17 02:56:33 +0800

    add alias bb for brew bundle

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,3 +3,4 @@ alias 2_alcatraz_store='cd ~/Library/Application\ Support/Alcatraz/Plug-ins'
 alias 2_xcodeplugins_store='cd ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins'
 alias ll="ls -AlTh"
 alias llrt="ls -rtAlTh"
+alias bb='brew bundle --global'
