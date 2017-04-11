be10415a41152d9b13c8ce133dc50221b410ccd6
duminghui <dumh@jolgoo.com>
2017-04-11 13:05:32 +0800

    modify Brewfile

--- gem/aliases.zsh
+++ gem/aliases.zsh
@@ -2,3 +2,6 @@
 alias gem_g_outdated='rvm @global do gem outdated -V'
 alias gem_g_update='rvm @global do gem update -V'
 alias gem_g_cleanup='rvm @global do gem cleanup -V'
+alias gem_outdated='gem outdated -V'
+alias gem_update='gem update -V'
+alias gem_cleanup='gem cleanup -V'
