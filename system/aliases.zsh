93c72a1895e1762c75fe0be0da4e5f42557ee232
duminghui <dumh@jolgoo.com>
2017-07-22 13:11:17 +0800

    fix alias ls in tmux

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,9 +3,10 @@ alias 2_alcatraz_store='cd ~/Library/Application\ Support/Alcatraz/Plug-ins'
 alias 2_xcodeplugins_store='cd ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins'
 alias ll="ls -AlTh"
 alias llrt="ls -rtAlTh"
+alias ls='ls -G'
 alias bb='brew bundle --global -v'
-alias pip_o='pip list -o --format=columns'
-alias pip_u='pip install --upgrade'
+alias pip2_o='pip2 list -o --format=columns'
+alias pip2_u='pip2 install --upgrade'
 alias pip3_o='pip3 list -o --format=columns'
 alias pip3_u='pip3 install --upgrade'
 alias proxy_on='export http_proxy=http://localhost:1087;export https_proxy=$http_proxy'
