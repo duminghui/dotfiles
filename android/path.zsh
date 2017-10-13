46148d1c8e4730b53d0f95810d333578a55ce602
duminghui <duminghui@users.noreply.github.com>
2017-10-13 12:54:16 +0800

    modify android ndk home

--- android/path.zsh
+++ android/path.zsh
@@ -3,4 +3,4 @@ export PATH=$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH
 export NDK_HOME=$ANDROID_HOME/ndk-bundle
 export ANDROID_NDK=$NDK_HOME
 export ANDROID_NDK_HOME=$NDK_HOME
-export PATH=$NDK_HOME/bin:$PATH
+export PATH=$NDK_HOME:$PATH
