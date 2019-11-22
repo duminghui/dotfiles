a8a491b34435dbe384c6643fb64e3dcf26cbe6b2
duminghui <duminghui@users.noreply.github.com>
2019-11-22 16:54:07 +0800

    modify flutter

--- flutter/envs.zsh
+++ flutter/envs.zsh
@@ -1,4 +1,8 @@
-export PUB_HOSTED_URL="https://mirrors.tuna.tsinghua.edu.cn/dart-pub/"
-export FLUTTER_STORAGE_BASE_URL="https://mirrors.tuna.tsinghua.edu.cn/flutter"
+# export PUB_HOSTED_URL="https://mirrors.tuna.tsinghua.edu.cn/dart-pub/"
+# export FLUTTER_STORAGE_BASE_URL="https://mirrors.tuna.tsinghua.edu.cn/flutter"
+export PUB_HOSTED_URL=https://pub.flutter-io.cn
+export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
+export PUB_CACHE=~/.pub-cache
+
 export PATH=~/Library/flutter/bin:$PATH
 
