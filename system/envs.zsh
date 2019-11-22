57eb0af5be4afcc8ff96bc280a40391db5fdea3a
duminghui <duminghui@139.com>
2019-11-22 16:54:07 +0800

    modify flutter

--- system/envs.zsh
+++ system/envs.zsh
@@ -1,3 +1,3 @@
 export LANG=zh_CN.UTF-8
 export LC_CTYPE=zh_CN.UTF-8
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+# export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
