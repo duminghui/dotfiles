54b56a960263d22658bc0d34fca3e0ea9ca5366f
dumh <duminghui@users.noreply.github.com>
2022-04-03 23:22:51 +0800

    add new config

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -4,14 +4,13 @@ alias 2_xcodeplugins_store='cd ~/Library/Application\ Support/Developer/Shared/X
 alias ll="ls -AlhT"
 alias llrt="ls -rtAlhT"
 alias ls='ls -G'
-alias bb='brew bundle --global -v'
-alias pip2_o='pip2 list -o --format=columns'
-alias pip2_u='pip2 install --upgrade'
+alias bbinstall='brew bundle --global -v'
+alias bbdump='brew bundle --global -v -f dump'
+alias bbclean='brew bundle --global -v cleanup'
+alias bblist='brew bundle --global -v list'
 alias pip3_o='pip3 list -o --format=columns'
 alias pip3_u='pip3 install --upgrade'
 alias proxy_on='export http_proxy=http://localhost:1087;export https_proxy=$http_proxy'
 alias proxy_off='unset http_proxy;unset https_proxy'
 alias proxy_ck="curl cip.cc"
 alias ipinfo='curl ip.cn'
-alias go_get_dep='go get -v -u github.com/golang/dep/cmd/dep'
-alias go_get_vg='go get -v -u github.com/GetStream/vg'
--- system/envs.zsh
+++ system/envs.zsh
@@ -1,4 +1,14 @@
 export LANG=zh_CN.UTF-8
 export LC_CTYPE=zh_CN.UTF-8
-export HOMEBREW_NO_AUTO_UPDATE=true
-# export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+
+# export HOMEBREW_NO_AUTO_UPDATE=true
+
+export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
+export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.ustc.edu.cn/homebrew-core.git"
+export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles"
+# brew tap --custom-remote --force-auto-update homebrew/cask https://mirrors.ustc.edu.cn/homebrew-cask.git
+
+# export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+# export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+# export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+# brew tap --custom-remote --force-auto-update homebrew/cask https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git
