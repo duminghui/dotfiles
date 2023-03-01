be6ba026af6248815d7a49ddaf1c9a30932f8b64
dumh <duminghui@users.noreply.github.com>
2023-03-01 22:27:30 +0800

    fix sccache_dir

--- rust/envs.zsh
+++ rust/envs.zsh
@@ -1,6 +1,6 @@
 export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
 export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
 export CARGO_HTTP_MULTIPLEXING=false
-# export SCCACHE_DIR=~/.cache/sccache
+export SCCACHE_DIR=~/.cache/sccache
 export SCCACHE_CACHE_SIZE="3G"
 
