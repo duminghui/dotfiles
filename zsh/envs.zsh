a2b7f024cc8b488756de9039b3caae379d776db7
dumh <duminghui@users.noreply.github.com>
2023-03-01 15:50:07 +0800

    modify zsh config

---
+++ zsh/envs.zsh
@@ -0,0 +1,4 @@
+[[ -s "/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]] && source "/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=246'
+bindkey '^s' autosuggest-accept
+
