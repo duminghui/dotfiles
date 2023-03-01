26a82f8012117492f2d00f734661a3fddd2eb49c
dumh <duminghui@users.noreply.github.com>
2023-03-01 14:12:50 +0800

    remove no use config

--- android/envs.zsh
+++ android/envs.zsh
@@ -3,4 +3,5 @@ export PATH=$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH
 export NDK_HOME=$ANDROID_HOME/ndk-bundle
 export ANDROID_NDK=$NDK_HOME
 export ANDROID_NDK_HOME=$NDK_HOME
+export NDK=$ANDROID_NDK_HOME
 export PATH=$NDK_HOME:$PATH
