ade975086ecee1d0c4d7e6406a7921dd536b954f
dumh <duminghui@users.noreply.github.com>
2023-03-02 21:26:56 +0800

    fix vscode-neovim

--- vscode-neovim/nvim/lua/xxx/core/log.lua
+++ vscode-neovim/nvim/lua/xxx/core/log.lua
@@ -3,40 +3,45 @@ local Log = {}
 ---@param msg any
 ---@param event any
 function Log:trace(msg, opts)
-    vim.notify(msg, vim.log.levels.TRACE, opts)
-    -- self:add_entry(self.levels.TRACE, msg, event)
+    vim.schedule(function()
+        vim.notify(msg, vim.log.levels.TRACE, opts)
+    end)
 end
 
 ---Add a log entry at DEBUG level
 ---@param msg any
 ---@param event any
 function Log:debug(msg, opts)
-    vim.notify(msg, vim.log.levels.DEBUG, opts)
-    -- self:add_entry(self.levels.DEBUG, msg, event)
+    vim.schedule(function()
+        vim.notify(msg, vim.log.levels.DEBUG, opts)
+    end)
 end
 
 ---Add a log entry at INFO level
 ---@param msg any
 ---@param event any
 function Log:info(msg, opts)
-    vim.notify(msg, vim.log.levels.INFO, opts)
-    -- self:add_entry(self.levels.INFO, msg, event)
+    vim.schedule(function()
+        vim.notify(msg, vim.log.levels.INFO, opts)
+    end)
 end
 
 ---Add a log entry at WARN level
 ---@param msg any
 ---@param event any
 function Log:warn(msg, opts)
-    vim.notify(msg, vim.log.levels.WARN, opts)
-    -- self:add_entry(self.levels.WARN, msg, event)
+    vim.schedule(function()
+        vim.notify(msg, vim.log.levels.WARN, opts)
+    end)
 end
 
 ---Add a log entry at ERROR level
 ---@param msg any
 ---@param event any
 function Log:error(msg, opts)
-    vim.notify(msg, vim.log.levels.ERROR, opts)
-    -- self:add_entry(self.levels.ERROR, msg, event)
+    vim.schedule(function()
+        vim.notify(msg, vim.log.levels.ERROR, opts)
+    end)
 end
 
 setmetatable({}, Log)
