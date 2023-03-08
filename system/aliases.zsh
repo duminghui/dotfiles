481bffc84d6f1059ad2fe1ff6ee9e7aaf6abdef8
dumh <duminghui@users.noreply.github.com>
2023-03-08 18:34:14 +0800

    modify alias

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -10,7 +10,6 @@ alias bbclean='brew bundle --global -v cleanup'
 alias bblist='brew bundle --global -v list'
 alias pip3_o='pip3 list -o --format=columns'
 alias pip3_u='pip3 install --upgrade'
-alias proxy_on='export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890'
-alias proxy_off='unset http_proxy;unset https_proxy;unset all_proxy'
-alias proxy_ck="curl cip.cc"
-alias ipinfo='curl ip.cn'
+alias proxy-on='export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890'
+alias proxy-off='unset http_proxy;unset https_proxy;unset all_proxy'
+alias ipinfo="curl cip.cc"
