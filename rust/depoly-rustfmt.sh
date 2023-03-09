8c0d3a2128e13f8a5bf16687edbc0ba72bd748df
dumh <duminghui@users.noreply.github.com>
2023-03-09 22:11:15 +0800

    modify deploy rustfmt

--- rust/depoly-rustfmt.sh
+++ rust/depoly-rustfmt.sh
@@ -3,4 +3,6 @@
 target_dir=~/Library/Application\ Support/rustfmt
 
 mkdir -pv "$target_dir"
-cp -v rustfmt.toml "$target_dir/rustfmt.toml"
+# cp -v rustfmt.toml "$target_dir/rustfmt.toml"
+ln -vfs "$(pwd)/rustfmt.toml" "$target_dir/rustfmt.toml"
+
