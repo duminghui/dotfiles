300330973c28d75a82fe5b2857a32276f4623f4c
dumh <duminghui@users.noreply.github.com>
2023-03-15 23:02:09 +0800

    1. fix some bug

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -29,7 +29,7 @@ set -gx HOMEBREW_CORE_GIT_REMOTE "https://mirrors.ustc.edu.cn/homebrew-core.git"
 set -gx HOMEBREW_BOTTLE_DOMAIN "https://mirrors.ustc.edu.cn/homebrew-bottles"
 set -gx HOMEBREW_API_DOMAIN "https://mirrors.ustc.edu.cn/homebrew-bottles/api"
 
-test -e /opt/homebrew/bin/brew; and eval "$(/opt/homebrew/bin/brew shellenv)"
+test -x /opt/homebrew/bin/brew; and eval "$(/opt/homebrew/bin/brew shellenv)"
 
 ### rust ####
 set -gx RUSTUP_DIST_SERVER https://mirrors.ustc.edu.cn/rust-static
@@ -39,8 +39,6 @@ set -gx SCCACHE_DIR ~/.cache/sccache
 set -gx SCCACHE_CACHE_SIZE "3G"
 
 set -gx PATH ~/.cargo/bin $PATH
-
-### oh-my-fish ###
 
 ### starship ###
 if type -q starship
@@ -66,11 +64,12 @@ if type -q exa
 end
 
 ### tmux ###
-# set -gx EDITOR nvim
-# set -gx DISABLE_AUTO_TITLE true
+type -q nvim; and set -gx EDITOR nvim
+# why add this in https://github.com/tmuxinator/tmuxinator
+set -gx DISABLE_AUTO_TITLE true
 
 ### golang ###
-if type -q brew
+if type -q brew; and test -d (brew --prefix)/opt/go/libexecx
     set -gx GOROOT (brew --prefix)/opt/go/libexec
     set -gx GOPATH $HOME/go
     set -gx GO111MODULE on
@@ -79,31 +78,31 @@ if type -q brew
 end
 
 ### gradle ###
-if type -q brew
+if type -q brew; and test -d (brew --prefix)/opt/gradle
     set -gx GRADLE_HOME (brew --prefix)/opt/gradle
     set -gx PATH $GRADLE_HOME/bin $PATH
 end
 
 ### android ###
 set -gx JAVA_HOME /Library/Java/Home
-set -gx PATH $JAVA_HOME/bin $PATH
+test -d $JAVA_HOME/bin; and set -gx PATH $JAVA_HOME/bin $PATH
 
 set -gx ANDROID_HOME ~/Library/Android/sdk
-set -gx PATH $ANDROID_HOME/tools $ANDROID_HOME/platform-tools $PATH
+test -d $ANDROID_HOME; and set -gx PATH $ANDROID_HOME/tools $ANDROID_HOME/platform-tools $PATH
 set -gx NDK_HOME $ANDROID_HOME/ndk-bundle
 set -gx ANDROID_NDK $NDK_HOME
 set -gx ANDROID_NDK_HOME $NDK_HOME
 set -gx NDK $ANDROID_NDK_HOME
-set -gx PATH $NDK_HOME $PATH
+test -d $NDK_HOME; and set -gx PATH $NDK_HOME $PATH
 
 ### flutter ###
 set -gx PUB_HOSTED_URL https://pub.flutter-io.cn
 set -gx PUB_CACHE ~/.pub-cache
 set -gx FLUTTER_STORAGE_BASE_URL https://storage.flutter-io.cn
-set -gx PATH ~/Library/flutter/bin $PATH
+test -d ~/Library/flutter/bin; and set -gx PATH ~/Library/flutter/bin $PATH
 
 ### nodejs ###
 set -gx  NVM_NODEJS_ORG_MIRROR https://npmmirror.com/mirrors/node/
 
-source /Users/dumh/.docker/init-fish.sh || true # Added by Docker Desktop
+# test -e ~/.docker/init-fish.sh; and source ~/.docker/init-fish.sh
 
