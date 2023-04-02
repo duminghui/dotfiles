51b7d732ceedf9f89aa0a3afc4e60f5918cf211c
dumh <duminghui@users.noreply.github.com>
2023-04-03 01:07:13 +0800

    add pnpm completion

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -50,4 +50,9 @@ set -gx PNPM_HOME "$XDG_DATA_HOME/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
 # pnpm end
+
