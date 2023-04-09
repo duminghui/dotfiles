3d28ee3e12a827bab86e98edb843a65e74e8d931
dumh <duminghui@users.noreply.github.com>
2023-04-09 23:17:04 +0800

    lsd modify icon

--- dot_config/lsd/executable_fix.sh
+++ dot_config/lsd/executable_fix.sh
@@ -138,6 +138,7 @@ sed -i "" -r \
     -e 's|﵂|󰡄|g' \
     -e 's||󰈹|g' \
     -e 's|gradle: 󰀲|gradle: |g' \
+    -e 's|config: 󱁿|config: 󰒓|g' \
     -e 's|||g' \
     icons.yaml
 
@@ -155,12 +156,22 @@ add_icon_to_name() {
   fi
 }
 
+add_icon_to_name "library" "󰁰"
 add_icon_to_name "movies" "󰎁"
-add_icon_to_name "\\.localized" "󰀵"
+add_icon_to_name "work" "󰃖"
 add_icon_to_name "android" "󰀲"
+add_icon_to_name "parallels" "󰜨"
+add_icon_to_name "fish" "󰈺"
+add_icon_to_name "tmux" "󰕮"
+add_icon_to_name "\\.localized" "󰀵"
+add_icon_to_name "\\.cache" "󰃨"
+add_icon_to_name "\\.docker" "󰡨"
 add_icon_to_name "\\.android" "󰀲"
 add_icon_to_name "\\.gradle" ""
-
+add_icon_to_name "\\.m2" ""
+add_icon_to_name "\\.condarc" "󰌠"
+add_icon_to_name "\\.yarnrc.yml" ""
+add_icon_to_name "\\.zshenv" "󰆍"
 
 
 # 添加新行命令添加到指令最后
