a93a8350adc291fbd2107bae7dfc379721efcee0
dumh <duminghui@users.noreply.github.com>
2023-04-10 15:07:45 +0800

    fix lsd

--- dot_config/lsd/executable_fix.sh
+++ dot_config/lsd/executable_fix.sh
@@ -2,7 +2,7 @@
 #
 # 󰅱 md-code_not_equal_variant
 
-sed -i "" -r \
+gsed -i -r \
     -e 's|^ {1,}\("(.*)",.*\)[, //]+"(.)"$|  \1: \2|g' \
     -e 's||󰆍|g' \
     -e 's|歷|󰒍|g' \
@@ -41,7 +41,7 @@ sed -i "" -r \
     -e 's||󰌞|g' \
     -e 's||󰗀|g' \
     -e 's||󰗰|g' \
-    -e 's|||g' \
+    -e 's||󰽭|g' \
     -e 's|פּ|󰙅|g' \
     -e 's||󰱾|g' \
     -e 's|||g' \
@@ -74,7 +74,7 @@ sed -i "" -r \
     -e 's||󰮥|g' \
     -e 's|||g' \
     -e 's||󰈰|g' \
-    -e 's||󰘦|g' \
+    -e 's||󰅩|g' \
     -e 's||󰖳|g' \
     -e 's|蘿|󰐑|g' \
     -e 's||󰙲|g' \
@@ -137,41 +137,58 @@ sed -i "" -r \
     -e 's|||g' \
     -e 's|﵂|󰡄|g' \
     -e 's||󰈹|g' \
-    -e 's|gradle: 󰀲|gradle: |g' \
-    -e 's|config: 󱁿|config: 󰒓|g' \
     -e 's|||g' \
     icons.yaml
 
-  #   -e '/^name:$/a \
-  # movies: 󰎁
-  #   ' \
-
-add_icon_to_name() {
-  local key=$1 icon=$2
-  if [ -z "$(sed -nr "/ +$key: $icon/p" icons.yaml)" ]; then
-    sed -i "" -r "/^name:\$/a \\
-  $key: $icon
-    " icons.yaml
-    echo "add icon config to name '  $key: $icon '"
+icon_manage() {
+  local rStart=$1 rEnd=$2 key=$3 icon=$4
+  if [ -z "$rStart" -o -z "$rEnd" -o -z "$key" -o -z "$icon" ]; then
+    echo "[Error] '$rStart', '$rEnd', '$key', '$icon'"
+    return
+  fi
+  local range="/^$rStart:\$/,/^$rEnd:\$"
+  if [ -n "$(gsed -nr "$range/{/^ +$key: $icon\$/p}" icons.yaml)" ]; then
+    return
+  elif [ -n "$(gsed -nr "$range/{/ +$key:/p}" icons.yaml)" ]; then
+    gsed -i -r "$range/{s|^ +$key: .*\$|  $key: $icon|g}" icons.yaml
+    echo "[]$rStart: '$key: $icon'"
+  else
+    gsed -i -r "/^$rStart:\$/a \ \ $key: $icon" icons.yaml
+    echo "[󰐕]$rStart: '$key: $icon'"
   fi
 }
 
-add_icon_to_name "library" "󰁰"
-add_icon_to_name "movies" "󰎁"
-add_icon_to_name "work" "󰃖"
-add_icon_to_name "android" "󰀲"
-add_icon_to_name "parallels" "󰜨"
-add_icon_to_name "fish" "󰈺"
-add_icon_to_name "tmux" "󰕮"
-add_icon_to_name "\\.localized" "󰀵"
-add_icon_to_name "\\.cache" "󰃨"
-add_icon_to_name "\\.docker" "󰡨"
-add_icon_to_name "\\.android" "󰀲"
-add_icon_to_name "\\.gradle" ""
-add_icon_to_name "\\.m2" ""
-add_icon_to_name "\\.condarc" "󰌠"
-add_icon_to_name "\\.yarnrc.yml" ""
-add_icon_to_name "\\.zshenv" "󰆍"
+name_icon(){
+  icon_manage 'name' 'extension' "$1" "$2"
+}
+
+extension_icon(){
+  icon_manage 'extension' 'filetype' "$1" "$2"
+}
+
+name_icon 'work' '󰃖'
+name_icon 'tmux' '󰕮'
+name_icon 'parallels' '󰜨'
+name_icon 'movies' '󰎁'
+name_icon 'library' '󰁰'
+name_icon 'gradle' ''
+name_icon 'fish' '󰈺'
+name_icon 'config' '󰒓'
+name_icon '"applications \(parallels\)"' '󰜨'
+name_icon 'android' '󰀲'
+name_icon '\.zshenv' '󰆍'
+name_icon '\.yarnrc.yml' ''
+name_icon '\.m2' ''
+name_icon '\.localized' '󰀵'
+name_icon '\.gradle' ''
+name_icon '\.docker' '󰡨'
+name_icon '\.condarc' '󰌠'
+name_icon '\.cache' '󰃨'
+name_icon '\.android' '󰀲'
+
+extension_icon 'jsonc' '󰘦'
+extension_icon 'gradle' ''
+extension_icon 'fish' '󰈺'
 
 
 # 添加新行命令添加到指令最后
