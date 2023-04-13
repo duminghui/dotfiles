650c0fa714b004dd48714ceeae484479cdbc3ac3
dumh <duminghui@users.noreply.github.com>
2023-04-13 16:07:20 +0800

    fix lsd icon

--- dot_config/lsd/executable_fix-icons.sh
+++ dot_config/lsd/executable_fix-icons.sh
@@ -3,11 +3,11 @@
 # https://github.com/lsd-rs/lsd/blob/master/src/theme/icon.rs
 # 󰅱 md-code_not_equal_variant
 
-cp icons-org.yaml icons.yaml
+cp icons.yaml.org icons.yaml
 
 gsed -i -r \
-    -e 's|^ +([a-z0-9._\-]+):.+[\/ ]+(.)$|  \1: \2|g' \
-    -e 's|^ +\("([A-Za-z0-9._+-]+)".+[\/ ]+"(.)"$|  \1: \2|g' \
+    -e 's|^ +([a-z_.-]+):.+[\/ ]+(\W{1})$|  \1: \2|g' \
+    -e 's|^ +\("([A-Za-z0-9._+-]+)".+[\/ ]+"(\W{1})"$|  \1: \2|g' \
     -e 's|symlink_dir|symlink-dir|g' \
     -e 's|symlink_file|symlink-file|g' \
     -e 's|device_char|device-char|g' \
@@ -202,6 +202,7 @@ name_icon '\.condarc' '󰌠'
 name_icon '\.cache' '󰃨'
 name_icon '\.android' '󰀲'
 
+extension_icon 'org' ''
 extension_icon 'jsonc' '󰘦'
 extension_icon 'jpg' '󰈥'
 extension_icon 'jpeg' '󰈥'
