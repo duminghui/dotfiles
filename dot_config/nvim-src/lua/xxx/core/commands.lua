015cca1db0a3ad6d570e29c90aa08c78ea5165c7
dumh <duminghui@users.noreply.github.com>
2023-06-06 16:31:27 +0800

    neovim fix rust

--- dot_config/nvim-src/lua/xxx/core/commands.lua
+++ dot_config/nvim-src/lua/xxx/core/commands.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-vim.cmd [[
+vim.cmd([[
   function! QuickFixToggle()
     if empty(filter(getwininfo(), 'v:val.quickfix'))
       copen
@@ -8,42 +8,41 @@ vim.cmd [[
       cclose
     endif
   endfunction
-]]
+]])
 
-local cmd_bdelete = "bdelete"
-if vim.fn.has "nvim-0.8" == 1 then
-    cmd_bdelete = "Bdelete"
-end
+local cmd_bdelete = 'bdelete'
+-- if vim.fn.has('nvim-0.8') == 1 then
+--   cmd_bdelete = 'Bdelete'
+-- end
 
 M.defaults = {
-    {
-        name = "BufferKill",
-        fn = function()
-            -- require("xxx.core.buffers").buf_kill "bd"
-            require("xxx.core.buffers").buf_kill(cmd_bdelete)
-        end,
-    },
-    {
-        name = "PrintRtp",
-        fn = function()
-            for k, v in pairs(vim.opt.rtp:get()) do
-                print(string.format("%3d: %s", k, v))
-            end
-        end
-    }
+  {
+    name = 'BufferKill',
+    fn = function()
+      -- require('xxx.core.buffers').buf_kill('bd')
+      require('xxx.core.buffers').buf_kill(cmd_bdelete)
+    end,
+  },
+  {
+    name = 'PrintRtp',
+    fn = function()
+      for k, v in pairs(vim.opt.rtp:get()) do
+        print(string.format('%3d: %s', k, v))
+      end
+    end,
+  },
 }
 
-
 function M.load(collection)
-    local common_opts = { force = true }
-    for _, cmd in pairs(collection) do
-        local opts = vim.tbl_deep_extend("force", common_opts, cmd.opts or {})
-        vim.api.nvim_create_user_command(cmd.name, cmd.fn, opts)
-    end
+  local common_opts = { force = true }
+  for _, cmd in pairs(collection) do
+    local opts = vim.tbl_deep_extend('force', common_opts, cmd.opts or {})
+    vim.api.nvim_create_user_command(cmd.name, cmd.fn, opts)
+  end
 end
 
 function M.load_defaults()
-    M.load(M.defaults)
+  M.load(M.defaults)
 end
 
 return M
--- dot_config/nvim-src/lua/xxx/lsp/providers/rust_analyzer.lua
+++ dot_config/nvim-src/lua/xxx/lsp/providers/rust_analyzer.lua
@@ -1,24 +1,24 @@
-local util = require "lspconfig.util"
-local lsp = require "xxx.lsp"
+local util = require('lspconfig.util')
+local lsp = require('xxx.lsp')
 
 local root_dir = nil
 
 -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/rust_analyzer.lua
 local function get_root_dir(fname)
-  local cargo_crate_dir = util.root_pattern "Cargo.toml"(fname)
-  local cmd = { "cargo", "metadata", "--no-deps", "--format-version", "1" }
+  local cargo_crate_dir = util.root_pattern('Cargo.toml')(fname)
+  local cmd = { 'cargo', 'metadata', '--no-deps', '--format-version', '1' }
   if cargo_crate_dir ~= nil then
-    cmd[#cmd + 1] = "--manifest-path"
-    cmd[#cmd + 1] = util.path.join(cargo_crate_dir, "Cargo.toml")
+    cmd[#cmd + 1] = '--manifest-path'
+    cmd[#cmd + 1] = util.path.join(cargo_crate_dir, 'Cargo.toml')
   end
-  local cargo_metadata = ""
-  local cargo_metadata_err = ""
+  local cargo_metadata = ''
+  local cargo_metadata_err = ''
   local cm = vim.fn.jobstart(cmd, {
     on_stdout = function(_, d, _)
-      cargo_metadata = table.concat(d, "\n")
+      cargo_metadata = table.concat(d, '\n')
     end,
     on_stderr = function(_, d, _)
-      cargo_metadata_err = table.concat(d, "\n")
+      cargo_metadata_err = table.concat(d, '\n')
     end,
     stdout_buffered = true,
     stderr_buffered = true,
@@ -30,23 +30,23 @@ local function get_root_dir(fname)
   end
   local cargo_workspace_dir = nil
   if cm == 0 then
-    cargo_workspace_dir = vim.json.decode(cargo_metadata)["workspace_root"]
+    cargo_workspace_dir = vim.json.decode(cargo_metadata)['workspace_root']
     if cargo_workspace_dir ~= nil then
       cargo_workspace_dir = util.path.sanitize(cargo_workspace_dir)
     end
   else
     vim.notify(
-      string.format("[XXX] cmd (%q) failed:\n%s", table.concat(cmd, " "), cargo_metadata_err),
+      string.format('[XXX] cmd (%q) failed:\n%s', table.concat(cmd, ' '), cargo_metadata_err),
       vim.log.levels.WARN
     )
   end
   return cargo_workspace_dir
     or cargo_crate_dir
-    or util.root_pattern "rust-project.json"(fname)
+    or util.root_pattern('rust-project.json')(fname)
     or util.find_git_ancestor(fname)
 end
 
-local opts = vim.tbl_deep_extend("keep", {
+local opts = vim.tbl_deep_extend('keep', {
   root_dir = function(fname)
     local function get()
       root_dir = get_root_dir(fname)
@@ -55,10 +55,10 @@ local opts = vim.tbl_deep_extend("keep", {
     return root_dir or get()
   end,
   settings = {
-    ["rust-analyzer"] = {
+    ['rust-analyzer'] = {
       -- https://rust-analyzer.github.io/manual.html#configuration
       rustfmt = {
-        extraArgs = { "+nightly" },
+        extraArgs = { '+nightly' },
         overrideCommand = nil,
         rangeFormatting = { enable = false },
       },
