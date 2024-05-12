27c313f88c523d025c022e3d2d70504ea398b62f
dumh <duminghui@users.noreply.github.com>
2024-05-12 22:37:43 +0800

    add miniconda

---
+++ miniconda/install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+set -eux
+MINICONDA_HOME=~/.local/share/miniconda3
+MINICONDA_DOWNLOAD_URL=https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-MacOSX-arm64.sh
+MINICONDA_DOWNLOAD_FILE=$MINICONDA_HOME/miniconda.sh
+
+mkdir -p $MINICONDA_HOME
+curl $MINICONDA_DOWNLOAD_URL -o $MINICONDA_DOWNLOAD_FILE
+bash $MINICONDA_DOWNLOAD_FILE -b -u -p $MINICONDA_HOME
+rm -rf $MINICONDA_DOWNLOAD_FILE
+
---
+++ miniconda/update.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+set -eux
+
+# https://github.com/conda/conda?tab=readme-ov-file#updating-conda
+conda update -n base conda
