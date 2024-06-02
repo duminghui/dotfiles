d88d7c0c50637441355ea6ea54c1d07c72bf7e85
dumh <duminghui@users.noreply.github.com>
2024-06-02 23:50:58 +0800

    输入法鼠须管配置更改

--- private_dot_config/nvim-src/lua/xxx/plugin-config/nvim-colorizer-rc.lua
+++ private_dot_config/nvim-src/lua/xxx/plugin-config/nvim-colorizer-rc.lua
@@ -2,6 +2,14 @@ local M = {}
 M.opts = {
   filetypes = { '*', '!mason', '!lazy' },
   user_default_options = {
+    RGB = true, -- #RGB hex codes
+    RRGGBB = true, -- #RRGGBB hex codes
+    names = true, -- "Name" codes like Blue or blue
+    RRGGBBAA = true, -- #RRGGBBAA hex codes
+    AARRGGBB = false, -- 0xAARRGGBB hex codes
+    mode = 'background',
+    -- mode = 'foreground',
+    -- mode = 'virtualtext',
     always_update = true,
   },
 }
