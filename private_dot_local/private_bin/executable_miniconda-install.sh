8c76cf2f574c49b88e7b988750285568e60c2f58
dumh <duminghui@users.noreply.github.com>
2024-08-26 20:48:53 +0800

    lsd modify icon, cargo modfiy source

--- private_dot_local/private_bin/executable_miniconda-install.sh
+++ private_dot_local/private_bin/executable_miniconda-install.sh
@@ -5,6 +5,10 @@ MINICONDA_DOWNLOAD_URL=https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/M
 MINICONDA_DOWNLOAD_FILE=$MINICONDA_HOME/miniconda3-latest-arm64.sh
 
 mkdir -p $MINICONDA_HOME
+
+cd $MINICONDA_HOME
+rm -rf .nonadmin _conda bin cmake conda-meta condabin etc include lib LICENSE.txt man pkgs python.app sbin share shell ssl
+
 curl $MINICONDA_DOWNLOAD_URL -o $MINICONDA_DOWNLOAD_FILE
 bash $MINICONDA_DOWNLOAD_FILE -b -u -p $MINICONDA_HOME
 rm -rf $MINICONDA_DOWNLOAD_FILE
