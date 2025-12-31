7a5b7adf618f77e3668974645e4c8a3c17afe406
dumh <duminghui@users.noreply.github.com>
2025-12-31 16:08:40 +0800

    modify fish config

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -16,21 +16,56 @@ if status is-interactive
     end
 end
 
+set --global fish_key_bindings fish_vi_key_bindings
+set --erase --universal fish_key_bindings
+
 # Emulates vim's cursor shape behavior
 # Set the normal and visual mode cursors to a block
 set fish_cursor_default block blink
 # Set the insert mode cursor to a line
-set fish_cursor_insert line blink
-# Set the replace mode cursor to an underscore
+set fish_cursor_insert underscore blink
+# Set the replace mode cursors to an underscore
 set fish_cursor_replace_one underscore blink
+set fish_cursor_replace underscore blink
+# Set the external cursor to a line. The external cursor appears when a command is started.
+# The cursor shape takes the value of fish_cursor_default when fish_cursor_external is not specified.
+set fish_cursor_external line blink
 # The following variable can be used to configure cursor shape in
 # visual mode, but due to fish_cursor_default, is redundant here
 set fish_cursor_visual underscore blink
 
-set fish_color_command green
-set fish_color_error magenta
-set fish_color_autosuggestion brgreen brblack
-# set fish_color_autosuggestion 586e75 brblack #brgreen
+fish_config theme choose my
+
+# set fish_color_command green
+# set fish_color_error magenta
+# set fish_color_autosuggestion brgreen
+# # set --global fish_color_autosuggestion brgreen --background=brblack
+# # set fish_color_autosuggestion 586e75 brblack #brgreen
+#
+# set --global fish_color_cancel -r
+# set --global fish_color_comment red
+# set --global fish_color_cwd green
+# set --global fish_color_cwd_root red
+# set --global fish_color_end green
+# set --global fish_color_escape brcyan
+# set --global fish_color_history_current --bold
+# set --global fish_color_host normal
+# set --global fish_color_host_remote yellow
+# set --global fish_color_normal normal
+# set --global fish_color_operator brcyan
+# set --global fish_color_param cyan
+# set --global fish_color_quote yellow
+# set --global fish_color_redirection cyan --bold
+# set --global fish_color_search_match white --background=brblack
+# set --global fish_color_selection white --bold --background=brblack
+# set --global fish_color_status red
+# set --global fish_color_user brgreen
+# set --global fish_color_valid_path --underline
+# set --global fish_pager_color_completion normal
+# set --global fish_pager_color_description yellow -i
+# set --global fish_pager_color_prefix normal --bold --underline
+# set --global fish_pager_color_progress brwhite --background=cyan
+# set --global fish_pager_color_selected_background -r
 
 ### starship ###
 if type -q starship
